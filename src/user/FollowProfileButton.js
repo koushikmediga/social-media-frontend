40725fe384a70158eb38adbc6897c12d3c855a3d
medigakoushik <medigasaikoushik@gmail.com>
2020-09-07 22:46:21 +0530

    the initial commit

---
+++ src/user/FollowProfileButton.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {follow,unfollow} from "./apiUser";
+
+class FollowProfileButton extends Component {
+    followClick = () => {
+        this.props.onButtonClick(follow);
+    };
+
+    unfollowClick = () => {
+        this.props.onButtonClick(unfollow);
+    };
+
+    render() {
+        return (
+            <div className="d-inline-block">
+            {
+                !this.props.following ? 
+                (
+                    <button onClick={this.followClick} className="btn btn-success btn-raised mr-5">
+                    follow
+                </button>
+                ) : (
+                    <button onClick={this.unfollowClick} className="btn btn-warning btn-raised">
+                    unfollow
+                </button>
+                
+                )
+            }
+             </div>   
+                
+        );
+    }
+}
+
+export default FollowProfileButton;
